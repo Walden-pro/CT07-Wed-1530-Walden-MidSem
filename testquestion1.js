62aeb6308b97d77139596e044caa60f73138de88
Walden-pro <waldenchho@gmail.com>
2026-09-02 08:00:32 +0000

    Sep 2, 2026, 8:00 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,11 +1,10 @@
 function setup(){
     new Canvas(600,400);
+    world.gravity.y = 10
 }
 function draw(){
     if(mouse.presses("left")){
         new Sprite(50,50,50);
         fill(random(0,225))
-        
-
     }
 }
