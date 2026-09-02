f9f20f8028138d91eff47c9af592475be9e802cd
Walden-pro <waldenchho@gmail.com>
2026-09-02 08:44:46 +0000

    Sep 2, 2026, 8:44 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,18 +1,14 @@
 function setup(){
     new Canvas(600,400);
     world.gravity.y = 10;
-    fill(230)
-    let base = new Sprite(300, 380, 600, 40);
+    let base = new Sprite(300, 380, 600, 30);
     base.collider = 'static';
 }
 function draw(){
 background(0)
     if (mouse.presses("left")){
-        
         fill(random(0,225));
-        
         let ball = new Sprite(mouse.x,mouse.y,40);
-        
         ball.bounciness = 0.5;
         ball.drag = 0.4;
     }
