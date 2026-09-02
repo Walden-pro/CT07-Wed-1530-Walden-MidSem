0ac51b9177d7735a0d8cdfc89d62dcfb9f81390a
Walden-pro <waldenchho@gmail.com>
2026-09-02 08:18:13 +0000

    Sep 2, 2026, 8:18 AM

--- testquestion1.js
+++ testquestion1.js
@@ -6,11 +6,12 @@ function setup(){
 }
 function draw(){
     if(mouse.presses("left")){
-        
+        spawnball()""
        
     }
 }
 function spawnball(){
+    fill(random(0,225));
     ball = new Sprite(mouse.x,mouse.y,50);
     ball.sleeping = false;
     ball.collider = 'dynamic';
