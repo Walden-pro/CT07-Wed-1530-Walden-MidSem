cc52940b4530735a2e05850853918b290a06de10
Walden-pro <waldenchho@gmail.com>
2026-09-02 08:24:33 +0000

    Sep 2, 2026, 8:24 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,6 +1,11 @@
 function setup(){
     new Canvas(600,400);
-    
+    world.gravity.y = 10;
+}
+function draw(){
+    if (mouse.presses("left")){
+        
+    }
 }
 // function spawnball(){
 //     fill(random(0,225));
