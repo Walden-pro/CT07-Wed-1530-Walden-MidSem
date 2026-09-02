cc5a31a567eae9537bebf9ce03a708e4383edb01
Walden-pro <waldenchho@gmail.com>
2026-09-02 08:21:41 +0000

    Sep 2, 2026, 8:21 AM

--- testquestion1.js
+++ testquestion1.js
@@ -6,14 +6,19 @@ function setup(){
 }
 function draw(){
     if(mouse.presses("left")){
-        spawnball();
+        fill(random(0,225));
+        new Canvas(600,400);
+        ball = new Sprite(mouse.x,mouse.y,50);
+        ball.sleeping = false;
+        ball.collider = 'dynamic';
+        ball.vel.y = 1;
     }
 }
-function spawnball(){
-    fill(random(0,225));
-    new Canvas(600,400);
-    ball = new Sprite(mouse.x,mouse.y,50);
-    ball.sleeping = false;
-    ball.collider = 'dynamic';
-    ball.vel.y = 1;
-}
+// function spawnball(){
+//     fill(random(0,225));
+//     new Canvas(600,400);
+//     ball = new Sprite(mouse.x,mouse.y,50);
+//     ball.sleeping = false;
+//     ball.collider = 'dynamic';
+//     ball.vel.y = 1;
+// }
