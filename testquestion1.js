8792eddac2c7c3929651cfead2784971efdd4929
Walden-pro <waldenchho@gmail.com>
2026-09-02 08:03:59 +0000

    Sep 2, 2026, 8:03 AM

--- testquestion1.js
+++ testquestion1.js
@@ -4,7 +4,7 @@ function setup(){
 }
 function draw(){
     if(mouse.presses("left")){
-        new Sprite(,mouse.x,mouse.y);
+        new Sprite(mouse.x,mouse.y,50);
         fill(random(0,225))
     }
 }
