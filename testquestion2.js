8a0901696f448a32506ffe2950a02c811dd1447c
Walden-pro <waldenchho@gmail.com>
2026-09-02 08:36:13 +0000

    Sep 2, 2026, 8:36 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,7 +1,7 @@
 function setup(){
     new Canvas(600,400);
     world.gravity.y = 10;
-    let base = new Sprite(0,600,1500,600);
+    let base = new Sprite(0,700,1500,700);
     base.collider = 'static';
 }
 function draw(){
